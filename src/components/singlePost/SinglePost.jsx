09c95bfb2be7d4ca3b73f863eff9dfd4afe51179
Kaviraghul <vettrithamil@gmail.com>
2022-01-13 01:18:32 +0530

    completed delete operation

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -1,13 +1,16 @@
 import {useLocation} from "react-router";
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext} from "react";
 import './singlePost.css';
 import axios from "axios";
+import { Context } from "../../context/Context"
+
 
 export default function SinglePost() {
     const location = useLocation();
     const path = location.pathname.split("/")[2];
     const [post, setPost] = useState({});
     const PF = "http://localhost:5000/images/"
+    const {user} = useContext(Context);
 
     useEffect(() => {
         const getPost = async () => {
@@ -16,6 +19,14 @@ export default function SinglePost() {
         };
         getPost();
     }, [path]);
+
+    const handleDelete = async () => {
+        try{
+            await axios.delete(`/posts/${post._id}`,{data:{username:user.username},});
+            window.location.replace("/");
+        }catch(err){}
+    }
+
     return (
         <div className='singlePost'>
             <div className="singlePostWrapper">
@@ -23,11 +34,13 @@ export default function SinglePost() {
                 {post.photo && (<img src={PF + post.photo} alt="" className="singlePostImg" />)}
             </div>
             <h1 className="singlePostTitle">
-            {post.title}  
-            <div className="singlePostEdit">
-                <i className=" singlePostIcon far fa-edit"></i>
-                <i className=" singlePostIcon far fa-trash-alt"></i>
-            </div>
+            {post.title} 
+            {post.username === user?.username && 
+                <div className="singlePostEdit">
+                    <i className=" singlePostIcon far fa-edit"></i>
+                    <i className=" singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
+                </div>
+            } 
             </h1>
             <div className="singlePostInfo">
                 <span className="singlePostAuthor">Author:<b>{post.username}</b></span>
